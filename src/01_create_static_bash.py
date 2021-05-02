5639d432fa23b9fbef832a53c7babe03cbada112
nakamura196 <na.kamura.1263@gmail.com>
2021-05-02 20:22:50 +0900

    add data

---
+++ src/01_create_static_bash.py
@@ -0,0 +1,55 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import csv
+from time import sleep
+import pandas as pd
+import json
+import urllib.request
+import os
+from PIL import Image
+import glob
+
+import yaml
+
+prefix = "http://nakamura196.github.io/iiif0"
+
+dir = "/Users/nakamurasatoru/git/d_dzi/a"
+
+# dir = "../docs/files/original"
+
+
+f = open('tmp/convert.sh', 'w')
+writer = csv.writer(f, lineterminator='\n')
+
+
+files = glob.glob(dir+'/docs/files/original/*.jpg', recursive=True)
+
+
+for i in range(len(files)):
+
+    file = files[i]
+
+    opath = file.replace("/original/", "/title/")
+    tmp = os.path.split(opath)
+    odir = tmp[0]
+
+    info_path = opath.replace(".jpg", "/info.json")
+
+    if not os.path.exists(info_path):
+        print(info_path)
+
+        # os.makedirs(odir, exist_ok=True)
+
+        line = "echo "+str(i+1)+"/"+str(len(files))
+        writer.writerow([line])
+
+        line = "mkdir -p "+odir
+        writer.writerow([line])
+
+        p = odir.replace(dir+"/docs", prefix)
+
+        line = "python iiif_static/iiif_static.py  -d "+odir+" -t 256  -p "+p+" "+file
+        writer.writerow([line])
+    
+
+f.close()
---
+++ src/tmp/convert.sh
@@ -0,0 +1,3 @@
+echo 2/2
+mkdir -p /Users/nakamurasatoru/git/d_dzi/a/docs/files/title
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/a/docs/files/title -t 256  -p http://nakamura196.github.io/toyo_images/files/title /Users/nakamurasatoru/git/d_dzi/a/docs/files/original/toyo.jpg
