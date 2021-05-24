32ba4cb6f02ccd82ed26f1140fe43a93eddd7b1e
nakamura196 <na.kamura.1263@gmail.com>
2021-05-24 17:35:05 +0900

    fix: data

--- src/tmp/convert.sh
+++ src/tmp/convert.sh
@@ -1,6 +1,3 @@
-echo 1/2
-mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile
-python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile -t 512  -p http://nakamura196.github.io/iiif0/files/tile /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/wakan.tif
 echo 2/2
 mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile
-python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile -t 512  -p http://nakamura196.github.io/iiif0/files/tile /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/tahara.tif
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile -t 256  -p https://nakamura196.github.io/iiif0/files/tile /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/tahara.tif
