e8c58b58ae0b3304331a6ced8c6f6b19af5874d7
nakamura196 <na.kamura.1263@gmail.com>
2021-08-03 20:48:19 +0900

    feat: add data

--- src/tmp/convert.sh
+++ src/tmp/convert.sh
@@ -1,3 +1,105 @@
-echo 2/13
-mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile
-python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile -t 256  -p https://nakamura196.github.io/iiif0/files/tile /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/img3_6_2.jpeg
+echo 8/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_35.jpg
+echo 9/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_21.jpg
+echo 10/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_20.jpg
+echo 11/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_34.jpg
+echo 12/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_22.jpg
+echo 13/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_23.jpg
+echo 14/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_27.jpg
+echo 15/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_33.jpg
+echo 16/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_32.jpg
+echo 17/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_26.jpg
+echo 18/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_30.jpg
+echo 19/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_24.jpg
+echo 20/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_18.jpg
+echo 21/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_19.jpg
+echo 22/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_25.jpg
+echo 23/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_31.jpg
+echo 24/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_9.jpg
+echo 25/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_8.jpg
+echo 26/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_1.jpg
+echo 27/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_3.jpg
+echo 28/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_2.jpg
+echo 29/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_6.jpg
+echo 30/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_7.jpg
+echo 31/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_5.jpg
+echo 32/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_4.jpg
+echo 33/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_28.jpg
+echo 34/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_14.jpg
+echo 35/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_15.jpg
+echo 36/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_29.jpg
+echo 37/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_17.jpg
+echo 38/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_16.jpg
+echo 39/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_12.jpg
+echo 40/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_13.jpg
+echo 41/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_11.jpg
+echo 42/48
+mkdir -p /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin
+python iiif_static/iiif_static.py  -d /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/tile/jyorin -t 256  -p https://nakamura196.github.io/iiif0/files/tile/jyorin /Users/nakamurasatoru/git/d_dzi/iiif0/docs/files/original/jyorin/img3_6_10.jpg
